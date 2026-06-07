ee9560062b35a66d3dea40bab3f7f3a3203c506e
chalds-mods <ttserafim9@gmail.com>
2026-06-07 16:42:44 +0700

    fix: cancel GameRenderer.renderBlur for SenseSwap screens

--- src/main/java/com/chalds/senseswap/mixin/NoBlurMixin.java
+++ src/main/java/com/chalds/senseswap/mixin/NoBlurMixin.java
@@ -3,19 +3,23 @@ package com.chalds.senseswap.mixin;
 import com.chalds.senseswap.gui.WheelSpinScreen;
 import com.chalds.senseswap.gui.DaySummaryScreen;
 import com.chalds.senseswap.gui.RolePopupScreen;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Screen.class)
+@Mixin(GameRenderer.class)
 public class NoBlurMixin {
-    @Inject(method = "method_57734", at = @At("HEAD"), cancellable = true)
-    private void cancelBlur(float blurRadius, CallbackInfo ci) {
-        if ((Object)this instanceof WheelSpinScreen ||
-            (Object)this instanceof DaySummaryScreen ||
-            (Object)this instanceof RolePopupScreen) {
+    @Shadow private MinecraftClient client;
+
+    @Inject(method = "method_57796", at = @At("HEAD"), cancellable = true)
+    private void cancelRenderBlur(float delta, CallbackInfo ci) {
+        if (client.currentScreen instanceof WheelSpinScreen ||
+            client.currentScreen instanceof DaySummaryScreen ||
+            client.currentScreen instanceof RolePopupScreen) {
             ci.cancel();
         }
     }
